040621b0dfff27e9cbb0b35e092df73c142761f0
miawcode <bararaff@gmail.com>
2025-03-06 05:34:30 +0700

    penambahan file number, boolean, dan string

---
+++ 3.boolean.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := true
+	y := false
+
+	fmt.Println("Hasil benar ialah", x)
+	fmt.Println("Hasil salah ialah", y)
+}
---
+++ 4.string.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Akbar")
+	fmt.Println("Rafsanjani")
+
+	fmt.Println(len("Halo"))
+	fmt.Println("lorem ipsum dolor sit amet"[6])
+}
